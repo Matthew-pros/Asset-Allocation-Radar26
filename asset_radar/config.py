6f240e6dc89714a3373739e9d4f8c4a1f6c2f9a8
MatthewAnders <105516556+Matthew-pros@users.noreply.github.com>
2026-02-02 06:30:12 +0100

    Create config.py

---
+++ asset_radar/config.py
@@ -0,0 +1,10 @@
+ASSETS = {
+    'SPY': ('US Akcie', 'Akcie'),
+    'EFA': ('Developed Markets', 'Akcie'),
+    'EEM': ('Emerging Markets', 'Akcie'),
+    'TLT': ('US Dluhopisy', 'Dluhopisy'),
+    'GLD': ('Zlato', 'Komodity'),
+    'DBC': ('Komodity', 'Komodity'),
+    'UUP': ('US Dolar', 'Měny'),
+    'VNQ': ('Reality', 'Reality'),
+}
